159ad0cdbb6a2edd046841a4476c9a87c16f5c02
matjamr <jamr.mat@gmail.com>
2025-10-07 09:08:19 +0200

    Initialize apps

---
+++ apps/backend/src/main/java/it/edu/pk/ztdbbackend/ZtdbBackendApplication.java
@@ -0,0 +1,13 @@
+package it.edu.pk.ztdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtdbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtdbBackendApplication.class, args);
+    }
+
+}
